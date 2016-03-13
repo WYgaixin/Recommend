bf0b0c8b53bdc72f8110eddd510ca09eebbc6405
WYgaixin <974160815@qq.com>
2016-03-13 22:26:25 +0800

    加了偏置向和map

--- src/MF.py
+++ src/MF.py
@@ -34,14 +34,33 @@ def InitLFM(F):
          if i not in q:
              q[i]=[random.random()/math.sqrt(F) for x in range(0,F)]
      return p,q
-         
+
+def InitLFMui(F):
+    for u,i,record in train:
+        bu[u]=0
+        bi[i]=0
+        if u not in p:
+            p[u]=[random.random()/math.sqrt(F) for x in range(0,F)]
+        if i not in q:
+            q[i]=[random.random()/math.sqrt(F) for x in range(0,F)]
+    return p,q,bu,bi
+      
 def Predict(u,i,F):
     ret=0.0
     for f in range(0,len(p[u])):
         ret+=p[u][f]*q[i][f]
     return ret
 
+def Predictui(u,i,F,bu,bi,mu):
+    ret=mu+bu[u]+bi[i]
+    ret+=sum(p[u][f]*q[i][f] for f in range(0,len(p[u])))
+    return ret
+
 def LearningLFM(F,n,alpha,lambd):
+    print "diedaicishu",n
+#    print "quangzhong",w
+    print "alpha",alpha
+    print "lambd",lambd
     p,q=InitLFM(F)
     for step in range(0,n):
         for u,i,record in train:
@@ -53,6 +72,27 @@ def LearningLFM(F,n,alpha,lambd):
         alpha*=0.9
     return p,q
 
+def LearingLFMui(F,n,alpha,lambd,mu):
+    print 'F:',F
+    print 'n:',n
+    print 'alpha:',alpha
+    print 'lambd:',lambd
+    p,q,bu,bi=InitLFMui(F)
+    for step in range(0,n):
+        for u,i,record in train:
+           
+            pui=Predictui(u,i,F,bu,bi,mu)
+            eui=record-pui
+            bu[u] +=alpha*(eui-lambd*bu[u])
+            bi[i] +=alpha*(eui-lambd*bi[i])
+            for k in range(0,F):
+                p[u][k]+=alpha*(q[i][k]*eui-lambd*p[u][k])
+                q[i][k]+=alpha*(p[u][k]*eui-lambd*q[i][k])
+        alpha*=0.9
+    return p,q,bu,bi
+                
+    
+
 def RMSE(F):
     rmse=0.0
     for u,i,record in test:
@@ -62,7 +102,15 @@ def RMSE(F):
             continue
     rmse = math.sqrt(rmse / float(len(test)))
     return rmse
-
+def RMSEui(F):
+    rmse=0.0
+    for u,i,record in test:
+        if((u in p) and (i in q)):
+            rmse+=float(math.pow((record-Predictui(u,i,F,bu,bi,mu)),2))
+        else:
+            continue
+    rmse=math.sqrt(rmse/float(len(test)))
+    return rmse 
 def MAE(F):
     mae=0.0
     for u,i,record in test:
@@ -72,6 +120,19 @@ def MAE(F):
             continue
     mae =mae/float(len(test))
     return mae
+
+def MAP(F):
+    mp=0.0
+    rc=0.0
+    for u,i,record in test:
+        if((u in p) and (i in q)):
+            mp+=float(math.fabs(record - Predict(u,i,F)))
+            rc+=record
+        else:
+            continue
+    mp =mp/rc
+    return mp
+            
     
         
 
@@ -81,10 +142,16 @@ if __name__ == '__main__':
     test=[]
     p=dict()
     q=dict()
+    bu=dict()
+    bi=dict()
     data,test,train=SplitData(8,1,1)
-    p,q=LearningLFM(80,5,0.005,0.5)
-    rmse=RMSE(5)
-    mae=MAE(5)
+    p,q=LearningLFM(10,5,0.005,0.5)
+    p,q,bu,bi=LearingLFMui(10,5,0.005,0.5,0)
+    rmse=RMSEui(5)
+#    mp=MAP(5)
+#    print 'mp:',mp
+#    rmse=RMSE(5)
+#    mae=MAE(5)
 #    InitLFM(50)
     print 'rmse',rmse
-    print 'mae',mae
+#    print 'mae',mae
